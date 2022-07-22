337852a4fb770414820ea4396d4ddf4a9c83f6c2
Stirling Hostetter <stirlhoss@proton.me>
2022-07-22 11:07:03 -0500

    Finish up facade creation

--- app/facades/member_facade.rb
+++ app/facades/member_facade.rb
@@ -1,7 +1,10 @@
 class MemberFacade
   class << self
-    def member_by_affiliation
-
+    def member_by_affiliation(affiliation)
+      parsed_json = MemberService.member_search_by_affiliation(affiliation)
+      parsed_json.map do |member|
+        Member.new(member)
+      end
     end
   end
 end
