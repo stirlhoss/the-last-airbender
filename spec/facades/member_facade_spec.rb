a2b4d007ba47ae67ec9c4e9a435fb3d9b35fd1c7
Stirling Hostetter <stirlhoss@proton.me>
2022-07-22 10:48:45 -0500

    Create test

---
+++ spec/facades/member_facade_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe MemberFacade do
+  describe 'member_by_affiliation' do
+    it 'searches through characters with a specific affiliation and turns them into poros' do
+
+    end
+  end
+end
