b2cef770835ac76f0f089d312d5710b4a4fb1fe7
Stirling Hostetter <stirlhoss@proton.me>
2022-07-22 11:45:02 -0500

    Basic test for the search result

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Search: :Index do
+
+  it 'shows a list of the characters that fall under that affiliation and all their details' do
+    visit '/'
+
+    click_button 'Search For Members'
+
+    save_and_open_page
+
+    expect(page).to have_content "Aang"
+  end
+end
